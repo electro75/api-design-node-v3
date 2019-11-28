768724719bf4cbcc5e1335929d977da92a83356b
Sahil Naikdhure <sanaikdhure@gmail.com>
2019-11-28 09:54:28 +0530

    adds solution for auth-lesson

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,65 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
 
-export const signin = async (req, res) => {}
+  if(!req.body.email || !req.body.password) {
+    return res.status(400).send({message: 'Email and password required'});
+  }
+
+  try {
+    const user = await User.create(req.body)
+    const token = newToken(user)
+    res.send({token})
+  } catch(err) {
+    console.error(err);
+    res.status(400).end();
+  }
+}
+
+export const signin = async (req, res) => {
+  if(!req.body.email || !req.body.password) {
+    return res.status(400).send({message: 'Email and password required'});
+  }
+
+  
+  const user = await User.findOne({email: req.body.email}).exec();
+  if(!user) {
+    return res.status(401).end();
+  }
+
+  try {
+    const match = user.checkPassword(req.body.password)
+    if(!match) {
+      return res.status(401).send({ message: "Incorrect Password" });
+    }
+
+    const token = newToken(user);
+    return res.status(200).send({token})
+  } catch(e) {
+    console.error(e);
+    return res.status(401).send({ message: "Incorrect Password" })
+  }
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  let token = req.headers.authorization.split('Bearer ')[1];
+
+  if(!token) {
+    res.status(401).send({message: "Token Missing"});
+  } 
+
+  try {
+    const payload = await verifyToken(token);
+    const user = await User.findById(payload.id)
+            .select("-password") // removes the password from the payload
+            .lean() // converts mongoose document to JSON format
+            .exec() // converts it into a proper promise.
+
+    req.user = user
+    next();
+  } catch(e) {
+    console.error(e);
+    return res.status(401).send({message: 'User not found'});
+  }
 }
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,87 +1,56 @@
 export const getOne = model => async (req, res) => {
-  try {
-    const doc = await model
-      .findOne({ createdBy: req.user._id, _id: req.params.id })
-      .lean()
-      .exec()
+  const id = req.params.id;
+  const userId = req.user_id;
 
-    if (!doc) {
-      return res.status(400).end()
-    }
-
-    res.status(200).json({ data: doc })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
+  const data = await model.findOne({_id: id, createdBy: userId}).exec();
+  if(!data) {
+    res.status(404).end()
   }
+
+  res.status(200).json({data})
 }
 
 export const getMany = model => async (req, res) => {
-  try {
-    const docs = await model
-      .find({ createdBy: req.user._id })
-      .lean()
-      .exec()
+  let userId = req.user_id;
 
-    res.status(200).json({ data: docs })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
+  const data = await model.find({createdBy: userId}).exec();
+  if(!data) {
+    res.status(404).end();
   }
+
+  res.status(200).send({data});
 }
 
 export const createOne = model => async (req, res) => {
-  const createdBy = req.user._id
-  try {
-    const doc = await model.create({ ...req.body, createdBy })
-    res.status(201).json({ data: doc })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
+  const data = req.body;
+
+  try{
+    const bool = await model.create(data, {new: true}).exec();
+    res.send({message: 'added'});
+  } catch(err) {
+    res.send(401).end();
   }
+  
+  
 }
 
 export const updateOne = model => async (req, res) => {
-  try {
-    const updatedDoc = await model
-      .findOneAndUpdate(
-        {
-          createdBy: req.user._id,
-          _id: req.params.id
-        },
-        req.body,
-        { new: true }
-      )
-      .lean()
-      .exec()
+ const id = req.params.id; 
+ const userId = req.user_id;
 
-    if (!updatedDoc) {
-      return res.status(400).end()
-    }
+ const data = await model.updateOne({ _id: id, createdBy: user_id }, req.body, {new: true})
 
-    res.status(200).json({ data: updatedDoc })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
-  }
+ if(!data) {
+   res.status(404).end();
+ } else {
+   res.status(201).json({data});
+ }
 }
 
 export const removeOne = model => async (req, res) => {
-  try {
-    const removed = await model.findOneAndRemove({
-      createdBy: req.user._id,
-      _id: req.params.id
-    })
+  const data = await model.delete({_id: req.params.id})
 
-    if (!removed) {
-      return res.status(400).end()
-    }
-
-    return res.status(200).json({ data: removed })
-  } catch (e) {
-    console.error(e)
-    res.status(400).end()
-  }
+  res.status(200).json({data});
 }
 
 export const crudControllers = model => ({
